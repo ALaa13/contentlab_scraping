7d876922e3c22e803c574ec41f1b5913ec0b937b
unknown <handsome.alaa@gmail.com>
2022-10-17 16:22:55 +0200

    beta

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 const workQueue = new queue('work', REDIS_URL)
 
 app.get('/', async (req, res) => {
-    //const job = await workQueue.add({url: process.env.URL})
-    res.json({id: "job.id"})
+    const job = await workQueue.add({url: process.env.URL})
+    res.json({id: job.id})
 })
 
 workQueue.on('global:completed', (jobId, result) => {
